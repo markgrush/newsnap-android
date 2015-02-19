639341a4ec8470c2305c8fc6c13210c6812c0839
mark <markgrush303@gmail.com>
2015-02-18 19:07:18 -0600

    added POST api calls in newsnap service

--- app/src/main/java/com/newsnap/services/NewsnapService.java
+++ app/src/main/java/com/newsnap/services/NewsnapService.java
@@ -5,8 +5,10 @@ import com.newsnap.items.ThreadPost;
 import java.util.List;
 
 import retrofit.Callback;
+import retrofit.http.Field;
 import retrofit.http.GET;
 import retrofit.http.Headers;
+import retrofit.http.POST;
 import retrofit.http.Path;
 
 /**
@@ -21,5 +23,15 @@ public interface NewsnapService {
     @GET("/{thread}")
     @Headers("Accept: application/json")
     public void getThread(@Path("thread") String threadId, Callback<List<ThreadPost>> callback);
+
+    @POST("/")
+    public void createNewThread(
+            @Field("op-name") String name, @Field("op-email") String email,
+            @Field("title") String title, @Field("news") String news);
+
+    @POST("/{id}")
+    public void createNewReply(
+            @Path("id") String id, @Field("replier-name") String name,
+            @Field("replier-email") String email, @Field("reply") String reply);
 
 }
